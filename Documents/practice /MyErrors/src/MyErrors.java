b09e5de1506b883f9cbb550e9ecd28eabfa401b4
behulum <behulummd2018@gmail.com>
2020-10-05 10:24:51 -0400

    first

---
+++ Documents/practice /MyErrors/src/MyErrors.java
@@ -0,0 +1,13 @@
+public class MyErrors {
+        public static void main (String[] args) {
+            int x = 0;
+            System.out.println("x is "+x);
+
+             x = (int) 15.2; // set x to 15.2
+            System.out.println("x is now 15");
+
+            int y; // set y to 1 more then x
+            y =  x + 1;
+            System.out.println("x and y are " + x + " and " + y );
+        }
+    }
